31e96a55e549f7f34084027a8be88814f28520f2
christianaldrich <pang.aldrich@gmail.com>
2024-06-17 18:39:48 +0700

    Initial Commit

---
+++ ChordJam/ChordJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordJamApp.swift
+//  ChordJam
+//
+//  Created by Christian Aldrich Darrien on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChordJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
